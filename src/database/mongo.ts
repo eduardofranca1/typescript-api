1858208040ae3129543091d3df33e1ce11774c89
eduardofranca1 <francaedu98@gmail.com>
2025-02-18 15:19:49 -0300

    chore: configure tsconfig file

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,4 +1,4 @@
-import { MongoClient as Mongo, Db } from "mongodb";
+import { MongoClient as Mongo, Db, Collection } from "mongodb";
 import {
   mongodb_database,
   mongodb_database_test,
@@ -29,30 +29,8 @@ export const MongoClient = {
       console.log("mongodb connection closed!");
     }
   },
-};
 
-export const MongoClientForTests = {
-  client: undefined as unknown as Mongo,
-  db: undefined as unknown as Db,
-
-  async connect(): Promise<void> {
-    const client = new Mongo(mongodb_url);
-    const db = client.db(mongodb_database_test);
-
-    this.client = client;
-    this.db = db;
-
-    console.log("connected to mongodb!");
-  },
-
-  async disconnect(): Promise<void> {
-    if (this.client) {
-      await this.client.close();
-
-      this.client = undefined as unknown as Mongo;
-      this.db = undefined as unknown as Db;
-
-      console.log("mongodb connection closed!");
-    }
+  getColletion(name: string): Collection {
+    return this.client.db().collection(name);
   },
 };
