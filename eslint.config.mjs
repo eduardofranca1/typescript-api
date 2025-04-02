7736c043ee217b3ca920bfa52f339fd98a6277b6
eduardofranca1 <francaedu98@gmail.com>
2025-04-02 16:15:39 -0300

    chore: configure eslint globalIgnores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,14 @@
+import { globalIgnores } from "eslint/config";
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  globalIgnores(["dist/**"]),
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    globalIgnores: ["dist"],
     files: ["**/*.{js,mjs,cjs,ts}"],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
