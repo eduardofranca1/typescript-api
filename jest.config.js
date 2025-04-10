d3c65d26c68897193a116043fe5d6c9c50a95e29
eduardofranca1 <francaedu98@gmail.com>
2025-04-10 13:28:46 -0300

    refactor: build config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,11 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@src/(.*)$": "<rootDir>/src/$1",
+    "^@tests/(.*)$": "<rootDir>/tests/$1",
+  },
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
   },
